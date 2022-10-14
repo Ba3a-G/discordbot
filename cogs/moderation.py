0344be66f552f404a6ff5ae96ac03d65f1e33c37
Aryan Kumar <aryan.ak434@gmail.com>
2022-10-15 02:35:59 +0530

    Add /invite command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -36,6 +36,15 @@ class Moderation(commands.Cog):
         await inter.guild.ban(member, reason=reason)
         await inter.send(f'Banned {member.name}{member.discriminator}!')
 
+    @commands.slash_command(
+        name='invite',
+        description='Get an invite link to this server.',
+        dm_permission=False
+    )
+    async def _invite(self, inter: disnake.CommandInter) -> None:
+        invite = await inter.channel.create_invite(max_age=0, max_uses=0)
+        await inter.send(f'Here\'s an invite link to this server: https://discord.gg/{invite.code}')
+
 
 def setup(bot):
     bot.add_cog(Moderation(bot))

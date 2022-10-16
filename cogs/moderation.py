3b964215d32a331f2f645da218d18154270b8bad
Aryan Kumar <aryan.ak434@gmail.com>
2022-10-16 06:25:47 +0530

    sync + reformat

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,8 +1,8 @@
 # cogs/moderation.py
 
-'''
+"""
 Moderation cog for commands.
-'''
+"""
 
 
 import disnake
@@ -103,6 +103,39 @@ class Moderation(commands.Cog):
     ) -> None:
         await inter.guild.kick(member, reason=reason)
         await inter.send(f"Kicked {member.name}{member.discriminator}!")
+
+    @commands.slash_command(
+        name="mkinvite",
+        description="Get an invite link to this server.",
+        dm_permission=False,
+    )
+    async def _mkinvite(self, inter: disnake.CommandInter) -> None:
+        invite = await inter.channel.create_invite(max_age=0, max_uses=0)
+        await inter.send(
+            f"Here's an invite link to this server: https://discord.gg/{invite.code}"
+        )
+
+    @commands.slash_command(
+        name="invites",
+        description="List all invites for this server.",
+        dm_permission=False,
+    )
+    async def _invites(self, inter: disnake.CommandInter) -> None:
+        invites = await inter.guild.invites()
+        embed = disnake.Embed(title="Invites")
+        if invites:
+            count = 1
+            for invite in invites:
+                link = f"https://discord.gg/{invite.code}"
+                embed.add_field(
+                    name=f"`{count}: {invite.code}`",
+                    value=f"Uses: {invite.uses} | Max Age: {invite.max_age} | [Link]({link})",
+                    inline=False,
+                )
+                count += 1
+        else:
+            embed.description = "No invites found."
+        await inter.send(embed=embed)
 
 
 def setup(bot):

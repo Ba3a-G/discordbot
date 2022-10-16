365641be3012ad0d2f966f3385c0ffd5869ca716
Aryan Kumar <aryan.ak434@gmail.com>
2022-10-16 06:14:21 +0530

    code reformat using black

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,8 +1,8 @@
 # cogs/moderation.py
 
-'''
+"""
 Moderation cog for commands.
-'''
+"""
 
 
 import disnake
@@ -15,56 +15,62 @@ class Moderation(commands.Cog):
         self.bot = bot
 
     @commands.slash_command(
-        name='ban',
-        description='Bans people.',
+        name="ban",
+        description="Bans people.",
         options=[
             Option(
-                'member',
-                'The server member you want to ban.',
+                "member",
+                "The server member you want to ban.",
                 OptionType.user,
-                required=True
+                required=True,
             ),
             Option(
-                'reason',
-                'Reason for banning the person.',
+                "reason",
+                "Reason for banning the person.",
                 OptionType.string
-            )
-        ]
+            ),
+        ],
     )
-    async def _ban(self, inter: disnake.CommandInter, member: disnake.Member,
-                   reason: str | None = None) -> None:
+    async def _ban(
+        self,
+        inter: disnake.CommandInter,
+        member: disnake.Member,
+        reason: str | None = None,
+    ) -> None:
         await inter.guild.ban(member, reason=reason)
-        await inter.send(f'Banned {member.name}{member.discriminator}!')
+        await inter.send(f"Banned {member.name}{member.discriminator}!")
 
     @commands.slash_command(
-        name='mkinvite',
-        description='Get an invite link to this server.',
-        dm_permission=False
+        name="mkinvite",
+        description="Get an invite link to this server.",
+        dm_permission=False,
     )
     async def _mkinvite(self, inter: disnake.CommandInter) -> None:
         invite = await inter.channel.create_invite(max_age=0, max_uses=0)
-        await inter.send(f'Here\'s an invite link to this server: https://discord.gg/{invite.code}')
+        await inter.send(
+            f"Here's an invite link to this server: https://discord.gg/{invite.code}"
+        )
 
     @commands.slash_command(
-        name='invites',
-        description='List all invites for this server.',
-        dm_permission=False
+        name="invites",
+        description="List all invites for this server.",
+        dm_permission=False,
     )
     async def _invites(self, inter: disnake.CommandInter) -> None:
         invites = await inter.guild.invites()
-        embed = disnake.Embed(title='Invites')
+        embed = disnake.Embed(title="Invites")
         if invites:
             count = 1
             for invite in invites:
-                link = f'https://discord.gg/{invite.code}'
+                link = f"https://discord.gg/{invite.code}"
                 embed.add_field(
-                    name=f'`{count}: {invite.code}`',
-                    value=f'Uses: {invite.uses} | Max Age: {invite.max_age} | [Link]({link})',
-                    inline=False
+                    name=f"`{count}: {invite.code}`",
+                    value=f"Uses: {invite.uses} | Max Age: {invite.max_age} | [Link]({link})",
+                    inline=False,
                 )
                 count += 1
         else:
-            embed.description = 'No invites found.'
+            embed.description = "No invites found."
         await inter.send(embed=embed)
 
 
